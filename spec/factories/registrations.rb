290cd045a0d7ddf85324407074bf9be3ec8a9d1f
AlysonBasilio <alyson.basilio@gmail.com>
2018-09-23 15:37:51 -0300

    added registrations tests

--- spec/factories/registrations.rb
+++ spec/factories/registrations.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :registration do
-    value { Faker::Number.positive.decimal(2) }
-    invoice_qty { Faker::Number.positive.number(2) }
-    expiration_date { Faker::Number.within(1..31) }
+    value { Faker::Number.decimal(2) }
+    invoice_qty { Faker::Number.number(2) }
+    expiration_date { Faker::Number.between(1, 31) }
     course { Faker::Lorem.word }
     institution_id { nil }
     student_id { nil }
---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+RSpec.describe 'Registrations API', type: :request do
+  # initialize test data
+  let!(:registrations) { create_list(:registration, 10) }
+  let(:registration_id) { registrations.first.id }
+
+  # Test suite for GET /registrations
+  describe 'GET /registrations' do
+    # make HTTP get request before each example
+    before { get '/registrations' }
+
+    it 'returns registrations' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /registrations/:id
+  describe 'GET /registrations/:id' do
+    before { get "/registrations/#{registration_id}" }
+
+    context 'when the record exists' do
+      it 'returns the registration' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(registration_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:registration_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Registration/)
+      end
+    end
+  end
+
+  # Test suite for POST /registrations
+  describe 'POST /registrations' do
+    # valid payload
+    let(:valid_attributes) { { name: 'ITA', cnpj: '01254789632', registration_type: 'Universidade' } }
+
+    context 'when the request is valid' do
+      before { post '/registrations', params: valid_attributes }
+
+      it 'creates a registration' do
+        expect(json['name']).to eq('ITA')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/registrations', params: { cnpj: '01251289632', registration_type: 'Universidade' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  # Test suite for PUT /registrations/:id
+  describe 'PUT /registrations/:id' do
+    let(:valid_attributes) { { name: 'UFC' } }
+
+    context 'when the record exists' do
+      before { put "/registrations/#{registration_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  # Test suite for DELETE /registrations/:id
+  describe 'DELETE /registrations/:id' do
+    before { delete "/registrations/#{registration_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+
+end
