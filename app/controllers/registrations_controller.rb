ce9adaf2b1aa2b6d4031485b43339340da29a8b8
AlysonBasilio <alyson.basilio@gmail.com>
2018-09-23 23:09:55 -0300

    creating invoices from registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,18 +10,35 @@ class RegistrationsController < ApplicationController
   # POST /registrations
   def create
     @registration = Registration.create!(registration_params)
+    $i = 0
+    $remaining_value = @registration.value
+    $today = Date.current()
+    if @registration.expiration_date > $today.day
+      $payment_day = Date.new($today.year, $today.month, @registration.expiration_date)
+    else
+      $today = $today.advance({months: 1})
+      $payment_day = Date.new($today.year, $today.month, @registration.expiration_date)
+    end
+    while $i < @registration.invoice_qty  do
+      if $i < @registration.invoice_qty - 1
+        $value = (@registration.value/@registration.invoice_qty).round(2)
+        $remaining_value -= $value
+      else
+        $value = $remaining_value
+      end
+
+      # Create Invoice
+      @registration.invoices.create({value: $value, expiration_date: $payment_day, registration_id: @registration.id})
+
+      $payment_day = $first_payment_day.advance({months: 1})
+      $i += 1
+    end
     json_response(@registration, :created)
   end
 
   # GET /registrations/:id
   def show
     json_response(@registration)
-  end
-
-  # PUT /registrations/:id
-  def update
-    @registration.update(registration_params)
-    head :no_content
   end
 
   # DELETE /registrations/:id
